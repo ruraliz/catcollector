a76388823d259248da048af6ccf59f2b3d5b347c
ruraliz <ruraliz12@gmail.com>
2022-12-03 15:47:27 -0800

    add: delete and update pages

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Cat
+from .models import Cat , Dog
 # Register your models here.
 admin.site.register(Cat)
+admin.site.register(Dog)
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 #Cat model that is connected to the Dtabase
 from .models import Cat
+# add these lines to the imports at the top
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
 
+class CatCreate(CreateView):
+  model = Cat
+  fields = '__all__'
+  success_url = '/cats'
+
+class CatUpdate(UpdateView):
+  model = Cat
+  fields = ['name', 'breed', 'description', 'age']
+
+  def form_valid(self, form):
+    self.object = form.save(commit=False)
+    self.object.save()
+    return HttpResponseRedirect('/cats/' + str(self.object.pk))
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats'
 
 # Create your views here.
 def index(request):
