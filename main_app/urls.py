cb84422873c8a311edb0c57c0491de8050422c63
ruraliz <ruraliz12@gmail.com>
2022-12-03 12:30:23 -0800

    add:show.html and user model

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('blog/', views.blog, name='blog'),
-     path('cats/', views.cats_index, name='cats'),
+    path('cats/', views.cats_index, name='cats'),
+    path('cats/<int:cat_id>/', views.cats_show, name= 'cats_show'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#Cat model that is connected to the Dtabase
+from .models import Cat
 
-# temp add Cats class
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-    def __str__(self):
-        return f"{self.name}"
-
-cats = [
-    Cat('Rufus', 'tabbycat', 'crazy cat', 103),
-    Cat('Simba', 'lion', 'brave', 5),
-    Cat('Garfield', 'tabbycat', 'likes lasagna', 43),
-]
-
-tanner = Cat('tanner', 'tabbycat', 'cool', 23)
 
 # Create your views here.
 def index(request):
+    cats= list(Cat.objects.all())
     return render(request, 'index.html', { 'cats': cats })
 
 def about(request):
@@ -34,4 +19,9 @@ def blog(request):
     return render(request, 'blog.html')
 
 def cats_index(request):
-    return render(request, 'cats/index.html' , {'cats': cats})   
+    cats = list(Cat.objects.all())
+    return render(request, 'cats/index.html' , {'cats': cats})
+def cats_show(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+
+    return render(request, 'cats/show.html',{'cat': cat})
